fdf163028c3db3c04ea2b093616530eab9fe78b4
matteoerrigo-dot <matteo.errigo@gmail.com>
2026-03-25 16:15:27 +0100

    Create cards.js

---
+++ data/cards.js
@@ -0,0 +1,19 @@
+export const cards = {
+  luffy: {
+    id: "luffy",
+    name: "Luffy",
+    cost: 3,
+    power: 4,
+    onReveal: "luffyBoost",
+    captainAbility: "crewBuff"
+  },
+
+  zoro: {
+    id: "zoro",
+    name: "Zoro",
+    cost: 2,
+    power: 3,
+    onReveal: "selfBuff",
+    captainAbility: "fighterBonus"
+  }
+};
